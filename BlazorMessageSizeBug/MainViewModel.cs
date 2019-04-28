53514bae49171fed82af0423bf69a8dee35e2658
Lee McPherson <leemcpherson@hotmail.com>
2019-04-28 11:37:06 -0700

    Add project files.

---
+++ BlazorMessageSizeBug/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMessageSizeBug
+{
+    public class MainViewModel
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public List<string> Data { get; set; } = new List<string>();
+
+        public MainViewModel(IJSRuntime jSRuntime)
+        {
+            _jsRuntime = jSRuntime;
+            
+        }
+
+        public async Task LoadDataAsync(int itemCount)
+        {
+            Data = await _jsRuntime.InvokeAsync<List<string>>("getData", itemCount);
+        }
+    }
+}
---
+++ BlazorMessageSizeBug/wwwroot/loadData.js
@@ -0,0 +1,16 @@
+﻿//function from https://stackoverflow.com/a/2117523/1938624
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+
+function getData(count) {
+    var data = [];
+    for (var i = 0; i < count; i++) {
+        data.push(uuidv4());
+    }
+    return data;
+}
